6c230310c1ba3b526541f0eb85e532bd479ac146
Vicka Svetlova <Vicka Svetlova>
2017-05-15 23:31:57 +0300

    init

---
+++ Assets/GraundTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GraundTest : MonoBehaviour {
+
+    #region Deligates
+    #endregion
+
+    #region Propertes
+    #endregion
+
+    #region Fields
+    #endregion
+
+    #region Events
+    public UnityEvent <bool> graunded;
+    #endregion
+
+    #region Metods
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag== "Graund")
+        {
+           graunded.Invoke(true);
+        }
+    }
+    #endregion
+}
+
+

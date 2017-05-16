3baa6ae8af77d6fe337cce1813e1c07c25ba75ae
vickasvetlova <vickasvetlova@gmail.com>
2017-05-16 18:37:04 +0300

    Controll  and move script

---
+++ Assets/Charecter/Script/CharecterController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharecterController : MonoBehaviour 
+{
+    #region Enums
+    #endregion
+
+    #region Delegates
+    #endregion
+
+    #region Structures
+    #endregion
+
+    #region Classes
+    #endregion
+
+    #region Fields
+    public Robot robot;
+    #endregion
+
+    #region Events
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    private void Start()
+    {
+        robot = robot == null ? GetComponent<Robot>() : robot;
+        if (robot == null)
+        {
+            Debug.LogError("Player not set to controller");
+        }
+    }
+    private void Update()
+    {
+        if (robot != null)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                robot.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                robot.MoveLeft();
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                robot.Jump();
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                robot.MoveDown();
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                robot.MoveUp();
+            }
+
+        }
+    }
+    #endregion
+
+    #region Event Handlers
+    #endregion
+}
---
+++ Assets/Charecter/Script/Robot.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    #region Enums
+    enum DirectionState
+    {
+        Left,
+        Right
+    }
+    enum MoveState
+    {
+        Idle,
+        Walk,
+        Jump,
+        Fall,
+        Dead
+
+    }
+    #endregion
+
+    #region Delegates
+    #endregion
+
+    #region Structures
+    #endregion
+
+    #region Classes
+    #endregion
+
+    #region Fields
+    #endregion
+
+    #region Events
+    #endregion
+
+    #region Properties
+    [Header("Speeds")]
+    public float WalkSpeed = 3;
+    public float JumpForce = 10;
+
+    private MoveState _moveState = MoveState.Idle;
+    private DirectionState _directionState = DirectionState.Right;
+    private Transform _transform;
+    private Rigidbody2D _rigbody;
+    private Animator _animatorController;
+    private float _walkTime = 0, _walkCooldawn = 0.02f;
+    #endregion
+
+    #region Methods
+    private void Start()
+    {
+        _animatorController = GetComponent<Animator>();
+        _rigbody = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        if (_moveState == MoveState.Jump)
+        {
+            if (_rigbody.velocity == Vector2.zero)
+            {
+                Idle();
+            }
+        }
+        else if (_moveState == MoveState.Walk)
+        {
+            _rigbody.velocity = ((_directionState == DirectionState.Right ? Vector2.right : -Vector2.right)
+                * WalkSpeed * Time.deltaTime);
+            _walkTime -= Time.deltaTime;
+            if (_walkTime <= 0)
+            {
+                Idle();
+            }
+
+        }
+
+    }
+    internal void MoveRight()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _moveState = MoveState.Walk;
+            if (_directionState == DirectionState.Left)
+            {
+                _transform.localScale = new Vector3(-transform.localScale.x, _transform.localScale.y, transform.localScale.z);
+                _directionState = DirectionState.Right;
+            }
+            _walkTime = _walkCooldawn;
+            _animatorController.Play("Walk");
+        }
+    }
+
+    internal void MoveLeft()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _moveState = MoveState.Walk;
+            if (_directionState == DirectionState.Right)
+            {
+                _transform.localScale = new Vector3(-transform.localScale.x, _transform.localScale.y, transform.localScale.z);
+                _directionState = DirectionState.Left;
+            }
+            _walkTime = _walkCooldawn;
+            _animatorController.Play("Walk");
+        }
+    }
+
+    internal void Jump()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _rigbody.velocity = (Vector3.up * JumpForce * Time.deltaTime);
+            _moveState = MoveState.Jump;
+            _animatorController.Play("Jump");
+        }
+    }
+
+    internal void MoveDown()
+    {
+        return;
+
+    }
+
+    internal void MoveUp()
+    {
+        return;
+
+    }
+
+    private void Idle()
+    {
+        _moveState= MoveState.Idle;
+        _animatorController.Play("Idle");
+    }
+    #endregion
+
+    #region Event Handlers
+    #endregion
+
+}
